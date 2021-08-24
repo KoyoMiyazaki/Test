499b12e301226cd8533f54c1915f6db87e8befa6
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-08-25 01:12:27 +0900

    update micropost app - update test files

--- docker_playground/django/rails_to_django/micropost_app/micropost/tests/test_views.py
+++ docker_playground/django/rails_to_django/micropost_app/micropost/tests/test_views.py
@@ -1,26 +1,35 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-class TestHomeView(TestCase):
+class BaseTestCase(TestCase):
+    """各種テストクラスの基底クラス"""
+    def setUp(self):
+        """テストメソッド実行前の事前設定"""
+        self.base_title = "Rails Tutorial Sample App"
+
+class TestHomeView(BaseTestCase):
     """HomeView用のテストクラス"""
 
     def test_should_get_home(self):
         """Homeページへのアクセスが成功することを検証する"""
         response = self.client.get(reverse_lazy('micropost:home'))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<title>Home | {}</title>'.format(self.base_title))
 
-class TestHelpView(TestCase):
+class TestHelpView(BaseTestCase):
     """HelpView用のテストクラス"""
 
     def test_should_get_help(self):
         """Helpページへのアクセスが成功することを検証する"""
         response = self.client.get(reverse_lazy('micropost:help'))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<title>Help | {}</title>'.format(self.base_title))
 
-class TestAboutView(TestCase):
+class TestAboutView(BaseTestCase):
     """AboutView用のテストクラス"""
 
     def test_should_get_about(self):
         """Aboutページへのアクセスが成功することを検証する"""
         response = self.client.get(reverse_lazy('micropost:about'))
         self.assertEqual(response.status_code, 200)
+        self.assertContains(response, '<title>About | {}</title>'.format(self.base_title))
