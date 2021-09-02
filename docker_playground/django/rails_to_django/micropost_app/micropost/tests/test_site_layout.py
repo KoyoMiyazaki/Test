fcbefec1c22efcc541aae7925eb883e6d25c3c73
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-09-03 01:40:57 +0900

    update micropost app - update templates and tests

---
+++ docker_playground/django/rails_to_django/micropost_app/micropost/tests/test_site_layout.py
@@ -0,0 +1,15 @@
+from django.test import SimpleTestCase
+from django.urls import reverse_lazy
+
+class TestSiteLayout(SimpleTestCase):
+    """サイトレイアウト用のテストクラス"""
+
+    def test_layout_links(self):
+        """サイト内のリンクを検証する"""
+        response = self.client.get(reverse_lazy('micropost:home'))
+        self.assertTemplateUsed(response, 'home.html')
+        self.assertContains(response, 'href="{}"'.format(reverse_lazy('micropost:home')), 2)
+        self.assertContains(response, 'href="{}"'.format(reverse_lazy('micropost:help')), 1)
+        self.assertContains(response, 'href="{}"'.format(reverse_lazy('micropost:about')), 1)
+        self.assertContains(response, 'href="{}"'.format(reverse_lazy('micropost:contact')), 1)
+        
