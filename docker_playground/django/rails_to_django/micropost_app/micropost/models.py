b347ab5708ded92c53c45e7f3f1779be8b18ed39
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-09-05 17:51:53 +0900

    update micropost app - update models

--- docker_playground/django/rails_to_django/micropost_app/micropost/models.py
+++ docker_playground/django/rails_to_django/micropost_app/micropost/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    """日記モデル"""
+
+    name = models.CharField(verbose_name='name', max_length=50)
+    email = models.EmailField(verbose_name='email', max_length=50)
+    created_at = models.DateTimeField(verbose_name='created_at', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='updated_at', auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'User'
