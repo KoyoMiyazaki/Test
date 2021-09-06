ad1f01b2ded295d4b333cc87c9777040e05208e8
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2021-09-06 22:44:15 +0900

    update micropost app - update tests

--- docker_playground/django/rails_to_django/micropost_app/micropost/models.py
+++ docker_playground/django/rails_to_django/micropost_app/micropost/models.py
@@ -1,12 +1,18 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 class User(models.Model):
-    """日記モデル"""
+    """ユーザモデル"""
 
-    name = models.CharField(verbose_name='name', max_length=50)
-    email = models.EmailField(verbose_name='email', max_length=50)
-    created_at = models.DateTimeField(verbose_name='created_at', auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name='updated_at', auto_now=True)
+    name = models.CharField(verbose_name='name',
+                            max_length=50)
+    email = models.EmailField(verbose_name='email',
+                              max_length=255,
+                              validators=[RegexValidator(r'^[\w+\-.]+@[a-zA-Z\d\-.]+\.[a-zA-Z]+$', 'メールアドレスの形式ではない')])
+    created_at = models.DateTimeField(verbose_name='created_at',
+                                      auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='updated_at',
+                                      auto_now=True)
 
     class Meta:
         verbose_name_plural = 'User'
---
+++ docker_playground/django/rails_to_django/micropost_app/micropost/tests/test_user_model.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.urls import reverse_lazy
+from django.db.utils import IntegrityError, DataError
+
+from ..models import User
+
+class TestUserModel(TestCase):
+    """UserModel用のテストクラス"""
+
+    def setUp(self):
+        """テストメソッド実行前の事前設定"""
+        self.user = User.objects.create(name="Test User", email="user@example.com")
+
+    def test_should_be_valid(self):
+        """Userモデルについて検証する"""
+        self.assertTrue(self.user)
+    
+    def test_name_should_be_present(self):
+        """Userモデルのnameについて、値をNoneに設定し妥当性を検証する"""
+        with self.assertRaisesMessage(IntegrityError, 'null value in column "name" of relation "micropost_user" violates not-null constraint'):
+            self.user.name = None
+            self.user.save()
+    
+    def test_email_should_be_present(self):
+        """Userモデルのemailについて、値をNoneに設定し妥当性を検証する"""
+        with self.assertRaisesMessage(IntegrityError, 'null value in column "email" of relation "micropost_user" violates not-null constraint'):
+            self.user.email = None
+            self.user.save()
+    
+    def test_name_should_not_be_too_long(self):
+        """Userモデルのnameについて、値を51文字以上に設定し妥当性を検証する"""
+        with self.assertRaisesMessage(DataError, 'value too long for type character'):
+            self.user.name = "a" * 51
+            self.user.save()
+    
+    def test_email_should_not_be_too_long(self):
+        """Userモデルのemailについて、値を256文字以上に設定し妥当性を検証する"""
+        with self.assertRaisesMessage(DataError, 'value too long for type character'):
+            self.user.email = "a" * 244 + "@example.com"
+            self.user.save()
